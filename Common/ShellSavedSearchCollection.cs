812a0ac10993be543a6722b180d84f861b318da8
Niklas Mollenhauer <nikeee@outlook.com>
2021-09-20 19:19:20 +0200

    Hi

---
+++ Common/ShellSavedSearchCollection.cs
@@ -0,0 +1,13 @@
+//Copyright (c) Microsoft Corporation.  All rights reserved.
+
+using MS.WindowsAPICodePack.Internal;
+
+namespace Microsoft.WindowsAPICodePack.Shell
+{
+    /// <summary>Represents a saved search</summary>
+    public class ShellSavedSearchCollection : ShellSearchCollection
+    {
+        internal ShellSavedSearchCollection(IShellItem2 shellItem)
+            : base(shellItem) => CoreHelpers.ThrowIfNotVista();
+    }
+}
